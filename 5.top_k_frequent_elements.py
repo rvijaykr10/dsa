fb559ba13bb6b2c933f986d5d6646a35dc28f130
Vijay Kumar <rvijaykr10@gmail.com>
2025-08-24 18:09:50 +0530

    implement topKFrequent function to find the k most frequent elements in a list

---
+++ 5.top_k_frequent_elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        result = []
+        hash = {}
+        for num in nums:
+            if num not in hash:
+                hash[num] = 1
+            else:
+                hash[num] += 1
+
+        res_list = list(hash.items())
+        res_sort = sorted(res_list,key=lambda x: x[1], reverse=True)
+
+        for n in res_sort:
+            if len(result) < k:
+                result.append(n[0])
+        return result
+        
