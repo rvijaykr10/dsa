8f2716c4fe3d7d0d77878619307f569e31ae4db9
Vijay Kumar <rvijaykr10@gmail.com>
2025-08-30 10:33:29 +0530

    add isPalindrome method to check if a string is a palindrome

---
+++ 9.valid_palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        formatted_string = "".join(c.lower() for c in s if c.isalnum())
+        if formatted_string == formatted_string[::-1]:
+            return True
+        return False
+
+
+print(Solution().isPalindrome("Was it a car or a cat I saw?"))  # True
